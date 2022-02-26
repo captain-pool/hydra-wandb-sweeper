e68de06d29118f605b936a182bef0d8a132f358d
Mattie Tesfaldet <mattie.tesfaldet@mila.quebec>
2022-02-25 22:20:58 -0500

    Compatibility with hydra experiment overrides. Quantized step for distributions now supported. Sweep parameterization now more aligned with Hydra's standardized approach. Some bug fixes. Sweep overrides now support RangeSweep properly via quantized uniform. Wandb run now directly passed to task function instead of run id to avoid unnecessary init overhead.

--- hydra_plugins/hydra_wandb_sweeper/config.py
+++ hydra_plugins/hydra_wandb_sweeper/config.py
@@ -1,27 +1,73 @@
-import dataclasses
-import typing
+from dataclasses import dataclass, field
+from typing import Any, Dict, MutableSequence, Optional
 
-import omegaconf
 from hydra.core import config_store
+from omegaconf import DictConfig
 
 
-@dataclasses.dataclass
+@dataclass
+class ScalarConfigSpec:
+    """Representation of all the options to define
+    a scalar.
+    """
+
+    # (int or float) Maximum and minimum values.
+    # if int, for int uniform-distributed hyperparameters. If float, for uniform-distributed hyperparameters.
+    min: Optional[float] = None
+    max: Optional[float] = None
+
+    # (float) Mean parameter for normal or log normal-distributed hyperparameters.
+    mu: Optional[float] = 0
+
+    # (float) Standard deviation parameter for normal or log normal-distributed hyperparameters.
+    sigma: Optional[float] = 1
+
+    # (float) Quantization step size for quantized hyperparameters.
+    q: Optional[float] = 1
+
+    # Specify how values will be distributed
+    # Supported distributions and required parameters
+    # "constant": ["value"],
+    # "categorical": ["values"],
+    # "int_uniform": ["min", "max"],
+    # "uniform": ["min", "max"],
+    # "q_uniform": ["min", "max"],
+    # "log_uniform": ["min", "max"],
+    # "q_log_uniform": ["min", "max"],
+    # "inv_log_uniform": ["min", "max"],
+    # "normal": ["mu", "sigma"],
+    # "q_normal": ["mu", "sigma"],
+    # "log_normal": ["mu", "sigma"],
+    # "q_log_normal": ["mu", "sigma"],
+    distribution: Optional[str] = None
+    value: Optional[float] = None
+    values: Optional[MutableSequence] = None
+
+
+@dataclass
 class WandbConfig:
     name: str
     method: str
-    count: typing.Optional[int] = None
-    metric: typing.Optional[omegaconf.DictConfig] = omegaconf.DictConfig({})
-    num_agents: typing.Optional[int] = 1
-    sweep_id: typing.Optional[str] = None
-    entity: typing.Optional[str] = None
-    project: typing.Optional[str] = None
-    early_terminate: typing.Optional[omegaconf.DictConfig] = omegaconf.DictConfig({})
+    count: Optional[int] = None
+    metric: Optional[DictConfig] = DictConfig({})
+    num_agents: Optional[int] = 1
+    sweep_id: Optional[str] = None
+    entity: Optional[str] = None
+    project: Optional[str] = None
+    early_terminate: Optional[DictConfig] = DictConfig({})
 
 
-@dataclasses.dataclass
+@dataclass
 class WandbSweeperConf:
     wandb_sweep_config: WandbConfig
     _target_: str = "hydra_plugins.hydra_wandb_sweeper.wandb_sweeper.WandbSweeper"
+
+    # default parametrization of the search space
+    # can be specified:
+    # - as a string, like commandline arguments
+    # - as a list, for categorical variables
+    # - as a full scalar specification
+    params: Dict[str, Any] = field(default_factory=dict)
 
 
 config_store.ConfigStore.instance().store(
--- hydra_plugins/hydra_wandb_sweeper/wandb_sweeper.py
+++ hydra_plugins/hydra_wandb_sweeper/wandb_sweeper.py
@@ -1,93 +1,173 @@
 import functools
 import itertools
 import logging
-import typing
+import os
+from pathlib import Path
+from typing import Any, Dict, List, MutableMapping, MutableSequence, Optional, Union
 
 import omegaconf
 import wandb
 from hydra.core import plugins
-from hydra.core.override_parser import overrides_parser, types
+from hydra.core.hydra_config import HydraConfig
+from hydra.core.override_parser import overrides_parser
+from hydra.core.override_parser.types import (
+    ChoiceSweep,
+    IntervalSweep,
+    Override,
+    RangeSweep,
+    Transformer,
+)
 from hydra.plugins import sweeper
+from omegaconf import DictConfig, ListConfig, OmegaConf
 
-from hydra_plugins.hydra_wandb_sweeper import config
+from hydra_plugins.hydra_wandb_sweeper.config import ScalarConfigSpec
 
 SUPPORTED_DISTRIBUTIONS = {
     "constant": ["value"],
     "categorical": ["values"],
     "int_uniform": ["min", "max"],
     "uniform": ["min", "max"],
-    "q_uniform": ["min", "max"],
+    "q_uniform": ["min", "max", "q"],
     "log_uniform": ["min", "max"],
-    "q_log_uniform": ["min", "max"],
+    "q_log_uniform": ["min", "max", "q"],
     "inv_log_uniform": ["min", "max"],
     "normal": ["mu", "sigma"],
-    "q_normal": ["mu", "sigma"],
+    "q_normal": ["mu", "sigma", "q"],
     "log_normal": ["mu", "sigma"],
-    "q_log_normal": ["mu", "sigma"],
+    "q_log_normal": ["mu", "sigma", "q"],
 }
 
 
-def get_wandb_parameter(override):
+def is_wandb_override(override: Override) -> bool:
+    if (
+        override.is_delete()
+        or override.is_add()
+        or override.is_force_add()
+        or override.is_hydra_override()
+    ):
+        return False
+    else:
+        return True
 
+
+def get_parameter(distribution, *args) -> Dict[str, Any]:
+    keys = SUPPORTED_DISTRIBUTIONS[distribution]
+    parameter = {"distribution": distribution}
+    for key, value in zip(keys, args):
+        if value is None:
+            raise TypeError(f"{key} must be assigned a value.")
+        if distribution not in ("constant", "categorical"):
+            if (key == "min" or key == "max") and "int" in distribution:
+                if not isinstance(value, int):
+                    raise TypeError(f"{value} assigned to {key} must be an integer.")
+            else:
+                value = float(value)
+        if distribution in "constant" and isinstance(value, MutableSequence):
+            assert len(value) == 1
+            value = value[0]
+        if isinstance(value, ListConfig):
+            value = OmegaConf.to_container(value, resolve=True)
+        parameter[key] = value
+    return parameter
+
+
+def create_wandb_param_from_config(
+    config: Union[MutableSequence[Any], MutableMapping[str, Any]]
+) -> Any:
+    if isinstance(config, MutableSequence):
+        if isinstance(config, ListConfig):
+            config = OmegaConf.to_container(config, resolve=True)
+        assert len(config) > 0
+        distribution = "constant" if len(config) == 1 else "categorical"
+        return get_parameter(distribution, config)
+    if isinstance(config, MutableMapping):
+        specs = ScalarConfigSpec(**config)
+        distribution = specs.distribution
+        if distribution not in SUPPORTED_DISTRIBUTIONS:
+            raise ValueError(
+                f"{distribution} not supported. "
+                f"Supported Distributions: {list(SUPPORTED_DISTRIBUTIONS.keys())}"
+            )
+        supported_params = SUPPORTED_DISTRIBUTIONS[distribution]
+        init_params = [getattr(specs, p) for p in supported_params]
+        param = get_parameter(specs.distribution, *init_params)
+        return param
+    param = get_parameter("constant", config)
+    return param
+
+
+def create_wandb_param_from_override(override: Override) -> Any:
     value = override.value()
-
-    def set_parameter(parameter, distribution, *args):
-        keys = SUPPORTED_DISTRIBUTIONS[distribution]
-        parameter["distribution"] = distribution
-        for key, value in zip(keys, args):
-            parameter[key] = value
-
     distribution = None
     if getattr(value, "tags", None):
-        assert len(value.tags) == 1
+        assert len(value.tags) == 1  # TODO: support 'grid' search method, i.e., no tags
         distribution = list(value.tags)[0]
         if distribution not in SUPPORTED_DISTRIBUTIONS:
             raise ValueError(
-                f"{distribution=} not supported. "
+                f"{distribution} not supported. "
                 f"Supported Distributions: {list(SUPPORTED_DISTRIBUTIONS.keys())}"
             )
-
-    parameter = {}
-
     if not override.is_sweep_override():
-        distribution = "constant"
-        set_parameter(parameter, distribution, value)
-
-        return parameter
-
+        return value
     if override.is_interval_sweep():
+        assert isinstance(value, IntervalSweep)
         distribution = distribution or "uniform"
-
-        if distribution in ("categorical", "constant"):
+        if "uniform" not in distribution or "q" in distribution:
             raise ValueError(
-                f"interval sweep doesn't support categorical/constant distribution"
+                f"Type IntervalSweep only supports non-quantized uniform distributions"
             )
-
-        set_parameter(parameter, distribution, value.start, value.end)
-        return parameter
-
-    if override.is_choice_sweep() or override.is_range_sweep():
-        choices = [
-            x for x in override.sweep_iterator(transformer=types.Transformer.encode)
-        ]
-
+        return get_parameter(
+            distribution, value.start, value.end
+        )  # TODO: support q tag
+    if override.is_choice_sweep():
+        assert isinstance(value, ChoiceSweep)
+        choices = [x for x in override.sweep_iterator(transformer=Transformer.encode)]
         distribution = distribution or "categorical"
-
         if distribution != "categorical":
+            raise ValueError(f"Type ChoiceSweep doesn't allow {distribution}")
+        return get_parameter(distribution, choices)
+    if override.is_range_sweep():
+        assert isinstance(value, RangeSweep)
+        distribution = distribution or "q_uniform"
+        if "uniform" not in distribution or "q" not in distribution:
             raise ValueError(
-                f"Choice Sweep and Range Sweep doesn't allow {distribution=}"
+                f"Type RangeSweep only supports quantized uniform distributions"
             )
-
-        set_parameter(parameter, distribution, choices)
-        return parameter
+        return get_parameter(distribution, value.start, value.end, value.step)
 
     raise NotImplementedError(f"{override} not supported by WandB sweeper")
 
 
+def _flatten_dict_gen(d, parent_key, sep):
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, MutableMapping):
+            yield from flatten_dict(v, new_key, sep=sep).items()
+        else:
+            yield new_key, v
+
+
+def flatten_dict(d: MutableMapping, parent_key: str = "", sep: str = "."):
+    return dict(_flatten_dict_gen(d, parent_key, sep))
+
+
+# TODO: support for 'grid' sweep method. If user provides 'grid' as the search
+# method, then we shouldn't be using SUPPORTED_DISTRIBUTIONS & "distribution"
+# and instead return "value" or "values" if the user provides a single value
+# or a list of values. This means we need to verify that "distribution" isn't
+# provided by the user in any of the params to be sweeped through.
 class WandbSweeper(sweeper.Sweeper):
-    def __init__(self, wandb_sweep_config: omegaconf.DictConfig) -> None:
+    def __init__(
+        self, wandb_sweep_config: omegaconf.DictConfig, params: Optional[DictConfig]
+    ) -> None:
         self.wandb_sweep_config = wandb_sweep_config
         self._sweep_id = None
+        self.params: Dict[str, Any] = {}
+        if params is not None:
+            assert isinstance(params, DictConfig)
+            self.params = {
+                str(x): create_wandb_param_from_config(y) for x, y in params.items()
+            }
 
         self.agent_run_count = self.wandb_sweep_config.get("count", None)
 
@@ -111,7 +191,7 @@ class WandbSweeper(sweeper.Sweeper):
         self.sweep_dict = {
             "name": self.wandb_sweep_config.name,
             "method": self.wandb_sweep_config.method,
-            "parameters": {},
+            "parameters": self.params,
         }
         early_terminate = self.wandb_sweep_config.early_terminate
         metric = self.wandb_sweep_config.metric
@@ -128,15 +208,21 @@ class WandbSweeper(sweeper.Sweeper):
     def sweep_id(self):
         return self._sweep_id
 
-    def sweep(self, arguments):
+    def sweep(self, arguments: List[str]) -> None:
         parser = overrides_parser.OverridesParser.create()
         parsed = parser.parse_overrides(arguments)
         sweep_id = self.wandb_sweep_config["sweep_id"]
 
-        parameters = self.sweep_dict["parameters"]
+        wandb_params = self.sweep_dict["parameters"]
+        hydra_overrides = []
 
         for override in parsed:
-            parameters[override.get_key_element()] = get_wandb_parameter(override)
+            if is_wandb_override(override):
+                wandb_params[
+                    override.get_key_element()
+                ] = create_wandb_param_from_override(override)
+            else:
+                hydra_overrides.append(override)
 
         if not sweep_id:
             sweep_id = wandb.sweep(
@@ -149,19 +235,45 @@ class WandbSweeper(sweeper.Sweeper):
         self._sweep_id = sweep_id
 
         overrides = []
-        for i in range(self.wandb_sweep_config.num_agents):
-            overrides.append(tuple(f"{key}=-1" for key in parameters))
+        for _ in range(self.wandb_sweep_config.num_agents):
+            overrides.append(
+                tuple(
+                    f"{override.get_key_element()}={override.value()}"
+                    for override in hydra_overrides
+                )
+            )
+        self.validate_batch_is_legal(overrides)
 
         self.launcher.launch(overrides, initial_job_idx=next(self.job_idx))
 
     def wandb_task(self, base_config, task_function, count):
         def run():
-            with wandb.init() as run:
-                run_id = run.id
-                override_config = run.config
-            override_config = omegaconf.DictConfig(override_config.as_dict())
-            config = omegaconf.OmegaConf.merge(base_config, override_config)
-            task_function(config, run_id)
+            runtime_cfg = HydraConfig.get()
+            logging.info("Initializing wandb")
+            with wandb.init(
+                name=Path(os.getcwd()).name,
+                settings=wandb.Settings(start_method="thread"),
+                notes=OmegaConf.to_yaml(runtime_cfg.overrides.task),
+                tags=[
+                    base_config.experiment.name,
+                    base_config.model._target_,
+                    base_config.dataset.name,
+                ],
+            ) as run:
+                override_dotlist = [
+                    f"{dot}={val}" for dot, val in run.config.as_dict().items()
+                ]
+                override_config = OmegaConf.from_dotlist(override_dotlist)
+                config = OmegaConf.merge(base_config, override_config)
+
+                # update any values not set by sweep (won't update those already configured by sweep)
+                config_dict = OmegaConf.to_container(
+                    config, resolve=True, throw_on_missing=True
+                )
+                config_dot_dict = flatten_dict(config_dict)
+                run.config.setdefaults(config_dot_dict)
+
+                task_function(config, run)
 
         if not self.sweep_id:
             raise ValueError(f"sweep_id cannot be {self.sweep_id}")
